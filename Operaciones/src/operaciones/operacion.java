a9aba333369496c71458ff5db95051342b883f2f
justyn <justinmesa2013@gmail.com>
2019-10-09 08:01:41 -0500

    proyecto arrays 2

--- Operaciones/src/operaciones/operacion.java
+++ Operaciones/src/operaciones/operacion.java
@@ -16,12 +16,44 @@ public class operacion {
     }
     //muestra el menu en pantalla
     public void mostrarMenu(){
-        System.out.println("1.Llenar Arrays\n"
+        System.out.println("\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n"
+        +"\n");
+        System.out.println("1.Leer Arrays\n"
                 + "2.Escribir arrays\n"
                 + "3.Sumatoria\n"
                 + "4.Productoria\n"
                 + "5.Tamaño del array\n"
-                + "6.Salir");
+                + "6.Salir\n"
+                + "-------------Seleccionar opcion-----------------");
         Scanner sc = new Scanner ( System.in );
         opcion = sc.nextInt();
         this.cargarOpcion(opcion);
@@ -29,18 +61,22 @@ public class operacion {
     //lee arreglo
     public void leerArreglo(){
         System.out.println("Su eleccion es 'leer arreglo'");
+        this.mostrarMenu();
     }
     //escribir arreglo
     public void ecribirArreglo(){
         System.out.println("Su eleccion es 'escribir arreglo'");
+        this.mostrarMenu();
     }
     //genera sumatoria
     public void sumatoria(){
         System.out.println("Su eleccion es 'sumatoria'");
+        this.mostrarMenu();
     }
     //genera productoria 
     public void productoria (){
         System.out.println("Su eleccion es 'productoria'");
+        this.mostrarMenu();
     }
     public void cargarOpcion (int op){
         switch(op){
